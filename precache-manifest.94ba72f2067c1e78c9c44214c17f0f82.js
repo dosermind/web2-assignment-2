6b4c11cbaca5bc2cdd8c761443bde86737807edd
BekoLolek <bekololek@gmail.com>
2023-04-23 18:10:01 +0200

    Updates

---
+++ precache-manifest.94ba72f2067c1e78c9c44214c17f0f82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e92dc4cb2563d3384ebc",
+    "url": "/BekoLolek/web2-assignment-2/static/css/main.0ee99ddb.chunk.css"
+  },
+  {
+    "revision": "e92dc4cb2563d3384ebc",
+    "url": "/BekoLolek/web2-assignment-2/static/js/main.e92dc4cb.chunk.js"
+  },
+  {
+    "revision": "62f0cfb8e08c452e00fd",
+    "url": "/BekoLolek/web2-assignment-2/static/js/1.62f0cfb8.chunk.js"
+  },
+  {
+    "revision": "e7b8b6aa65cec2605e59",
+    "url": "/BekoLolek/web2-assignment-2/static/js/runtime~main.e7b8b6aa.js"
+  },
+  {
+    "revision": "a9709cc4dcb367988e8b930c2f19cf32",
+    "url": "/BekoLolek/web2-assignment-2/index.html"
+  }
+];
